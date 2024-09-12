ca752d2e2ed13e1f16032cc9f3ff3cf37b6eb91f
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-12 13:40:48 +0300

    feat: add task Functionality and Set local Storage Environment

--- script.js
+++ script.js
@@ -3,4 +3,58 @@ import { darkThemeToggleElement } from "./scripts/elements";
 //DArk Theme
 darkThemeToggleElement.addEventListener("click", toggleTheme);
 
-const addTask = () => {};
+// Add Task
+const addButton = document.querySelector(".TaskSearchBar__button");
+
+const fetchData = (key) => {
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : false;
+};
+
+const addTask = (e) => {
+  e.preventDefault();
+  const inputElement = document.querySelector("#search-input");
+  let taskValue = inputElement.value;
+
+  if (!taskValue) return;
+
+  const task = {
+    value: taskValue,
+    isCompleted: false,
+  };
+
+  const tasks = fetchData("tasks") || [];
+
+  tasks.push(task);
+  saveToDb("tasks", tasks);
+
+  const taskList = document.querySelector(".TaskList__list");
+
+  let alltaskList = "";
+  tasks.forEach((task) => {
+    alltaskList += `<li class="TaskList__taskContent${
+      task.isCompleted ? " TaskList__taskContent--isActive" : ""
+    }" >
+      <div class='TaskList__checkbox' tabindex="0" role="button">
+        <img class='TaskList__checkboxImg' src="./assets/icon-checkmark.svg" alt="checkmark" />
+      </div>
+      <div class='TaskList__valueContent'>
+        <p class='TaskList__value'>
+          ${task.value}
+        </p>
+        <img src="./assets/icon-basket.svg"
+             class='TaskList__deleteIcon'
+             alt="basket-icon"
+        />
+      </div>
+    </li>`;
+  });
+  taskList.innerHTML = alltaskList;
+  inputElement.value = "";
+};
+
+const saveToDb = (key, data) => {
+  localStorage.setItem(key, JSON.stringify(data));
+};
+
+addButton.addEventListener("click", addTask);
