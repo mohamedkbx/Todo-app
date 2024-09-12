20c371398f499edc8d6d0f55701bae142058affc
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-12 14:35:05 +0300

    refactor : refactor Add task Function and make it pure

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 import toggleTheme from "./scripts/darkTheme";
-import { darkThemeToggleElement } from "./scripts/elements";
+import { darkThemeToggleElement, inputElement, taskList } from "./scripts/elements";
 //DArk Theme
 darkThemeToggleElement.addEventListener("click", toggleTheme);
-
 // Add Task
 const addButton = document.querySelector(".TaskSearchBar__button");
 
@@ -11,50 +10,57 @@ const fetchData = (key) => {
   return data ? JSON.parse(data) : false;
 };
 
-const addTask = (e) => {
-  e.preventDefault();
-  const inputElement = document.querySelector("#search-input");
-  let taskValue = inputElement.value;
+const saveToDb = (key, data) => {
+  localStorage.setItem(key, JSON.stringify(data));
+};
 
-  if (!taskValue) return;
-
-  const task = {
-    value: taskValue,
-    isCompleted: false,
-  };
-
-  const tasks = fetchData("tasks") || [];
-
-  tasks.push(task);
-  saveToDb("tasks", tasks);
-
-  const taskList = document.querySelector(".TaskList__list");
-
+const renderTaskList = (tasks) => {
+  // render Tasks Ui
   let alltaskList = "";
   tasks.forEach((task) => {
     alltaskList += `<li class="TaskList__taskContent${
       task.isCompleted ? " TaskList__taskContent--isActive" : ""
     }" >
-      <div class='TaskList__checkbox' tabindex="0" role="button">
-        <img class='TaskList__checkboxImg' src="./assets/icon-checkmark.svg" alt="checkmark" />
-      </div>
-      <div class='TaskList__valueContent'>
-        <p class='TaskList__value'>
-          ${task.value}
-        </p>
-        <img src="./assets/icon-basket.svg"
-             class='TaskList__deleteIcon'
-             alt="basket-icon"
-        />
-      </div>
-    </li>`;
+        <div class='TaskList__checkbox' tabindex="0" role="button">
+          <img class='TaskList__checkboxImg' src="./assets/icon-checkmark.svg" alt="checkmark" />
+        </div>
+        <div class='TaskList__valueContent'>
+          <p class='TaskList__value'>
+            ${task.value}
+          </p>
+          <img src="./assets/icon-basket.svg"
+               class='TaskList__deleteIcon'
+               alt="basket-icon"
+          />
+        </div>
+      </li>`;
   });
   taskList.innerHTML = alltaskList;
   inputElement.value = "";
+  inputElement.focus();
 };
 
-const saveToDb = (key, data) => {
-  localStorage.setItem(key, JSON.stringify(data));
+// add task
+const addTask = (e) => {
+  e.preventDefault();
+
+  let taskValue = inputElement.value;
+  // test the value
+  if (!taskValue) return;
+  // task component
+  const task = {
+    value: taskValue,
+    isCompleted: true,
+  };
+
+  //store & fetch the Tasks
+  const tasks = fetchData("tasks") || [];
+
+  tasks.push(task);
+  saveToDb("tasks", tasks);
+
+  renderTaskList(tasks);
 };
 
+// add task event
 addButton.addEventListener("click", addTask);
--- scripts/elements.js
+++ scripts/elements.js
@@ -2,3 +2,6 @@
 export const darkThemeToggleElement = document.querySelector(".DarkThemeToggle");
 //App Element
 export const appElement = document.querySelector(".App");
+// add Task Elements
+export const inputElement = document.querySelector("#search-input");
+export const taskList = document.querySelector(".TaskList__list");
